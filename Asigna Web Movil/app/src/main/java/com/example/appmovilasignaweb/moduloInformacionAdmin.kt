38589596b69b08b76a3e9facc5881c457bb3d7b1
Mariana Charry <marianamayaya@gmail.com>
2024-10-07 13:44:52 -0500

    Cambios

--- Asigna Web Movil/app/src/main/java/com/example/appmovilasignaweb/moduloInformacionAdmin.kt
+++ Asigna Web Movil/app/src/main/java/com/example/appmovilasignaweb/moduloInformacionAdmin.kt
@@ -1,6 +1,8 @@
 package com.example.appmovilasignaweb
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class moduloInformacionAdmin : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun irAgregarReserva(view: View) {
+        val intent = Intent(application, Crearreserva::class.java)
+        startActivity(intent)
     }
 }
