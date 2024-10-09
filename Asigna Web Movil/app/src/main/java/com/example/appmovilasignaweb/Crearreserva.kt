761bad8b703134a3cc13bfe28279523ec64b0b8e
kevin <kevincamilo1211@gmail.com>
2024-10-09 14:53:04 -0500

    sirve el reservar

--- Asigna Web Movil/app/src/main/java/com/example/appmovilasignaweb/Crearreserva.kt
+++ Asigna Web Movil/app/src/main/java/com/example/appmovilasignaweb/Crearreserva.kt
@@ -19,15 +19,13 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.appmovilasignaweb.config.config
 import org.json.JSONObject
-import java.lang.Exception
-import java.text.ParseException
 import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Locale
+import java.util.*
 
 class Crearreserva : Fragment() {
 
     private lateinit var txtNombre_completo: EditText
+    private lateinit var txtusername: EditText  // Campo para el correo electrónico
     private lateinit var txtNombre_espacio: Spinner
     private lateinit var txtFecha_entrada: EditText
     private lateinit var txtFecha_salida: EditText
@@ -35,29 +33,38 @@ class Crearreserva : Fragment() {
     private lateinit var txtHora_salida: EditText
     private lateinit var btnGuardar: Button
 
+    private var fechaEntrada: Calendar? = null
+    private var fechaSalida: Calendar? = null
+    private var horaEntrada: Calendar? = null
+    private var horaSalida: Calendar? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_crearreserva, container, false)
 
-        // Obtener las referencias de los botones y EditText
-        val btnCalendario = view.findViewById<ImageView>(R.id.btnCalendario)
-        val btnCalendario2 = view.findViewById<ImageView>(R.id.btnCalendario2)
+        // Inicializar las referencias de los componentes de la interfaz
+        txtNombre_completo = view.findViewById(R.id.txtNombre_completo)
+        txtusername = view.findViewById(R.id.txtusername)  // Inicialización del nuevo campo
         txtFecha_entrada = view.findViewById(R.id.txtFecha_entrada)
         txtFecha_salida = view.findViewById(R.id.txtFecha_salida)
         txtHora_entrada = view.findViewById(R.id.txtHora_entrada)
         txtHora_salida = view.findViewById(R.id.txtHora_salida)
         btnGuardar = view.findViewById(R.id.btnGuardar)
 
-        // Configurar el Spinner
+        // Inicializar los botones de los calendarios
+        val btnCalendario = view.findViewById<ImageView>(R.id.btnCalendario)
+        val btnCalendario2 = view.findViewById<ImageView>(R.id.btnCalendario2)
+
+        // Configurar el Spinner con las opciones
         txtNombre_espacio = view.findViewById(R.id.SpinnerNombreEspacio)
         val opciones = arrayOf("cancha", "gimnasio", "auditorio", "biblioteca")
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, opciones)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         txtNombre_espacio.adapter = adapter
 
-        // Asignar los listeners de clic a los botones
+        // Asignar listeners a los botones de calendario y de selección de hora
         btnCalendario.setOnClickListener {
             mostrarCalendario(txtFecha_entrada, true)
         }
@@ -74,6 +81,7 @@ class Crearreserva : Fragment() {
             mostrarHora(txtHora_salida, false)
         }
 
+        // Asignar listener al botón de guardar
         btnGuardar.setOnClickListener {
             crearReserva()
         }
@@ -85,6 +93,7 @@ class Crearreserva : Fragment() {
         try {
             // Verifica que los campos no estén vacíos
             if (txtNombre_completo.text.isEmpty() ||
+                txtusername.text.isEmpty() ||  // Verificar si el campo de username está vacío
                 txtFecha_entrada.text.isEmpty() ||
                 txtFecha_salida.text.isEmpty() ||
                 txtHora_entrada.text.isEmpty() ||
@@ -93,9 +102,30 @@ class Crearreserva : Fragment() {
                 return
             }
 
+            // Verifica que la fecha y hora de salida no sea anterior a la de entrada
+            if (fechaEntrada != null && fechaSalida != null && horaEntrada != null && horaSalida != null) {
+                val fechaHoraEntrada = Calendar.getInstance().apply {
+                    time = fechaEntrada!!.time
+                    set(Calendar.HOUR_OF_DAY, horaEntrada!!.get(Calendar.HOUR_OF_DAY))
+                    set(Calendar.MINUTE, horaEntrada!!.get(Calendar.MINUTE))
+                }
+
+                val fechaHoraSalida = Calendar.getInstance().apply {
+                    time = fechaSalida!!.time
+                    set(Calendar.HOUR_OF_DAY, horaSalida!!.get(Calendar.HOUR_OF_DAY))
+                    set(Calendar.MINUTE, horaSalida!!.get(Calendar.MINUTE))
+                }
+
+                if (fechaHoraSalida.before(fechaHoraEntrada)) {
+                    Toast.makeText(context, "La hora de salida no puede ser anterior a la de entrada.", Toast.LENGTH_SHORT).show()
+                    return
+                }
+            }
+
             // Crear el objeto JSON con los parámetros a enviar
             val parametros = JSONObject().apply {
                 put("nombre_completo", txtNombre_completo.text.toString())
+                put("username", txtusername.text.toString())  // Agregar el campo de username
                 put("nombre_espacio", txtNombre_espacio.selectedItem.toString())
                 put("fecha_entrada", txtFecha_entrada.text.toString())
                 put("fecha_salida", txtFecha_salida.text.toString())
@@ -109,11 +139,9 @@ class Crearreserva : Fragment() {
                 config.urlcrearReserva,  // Aquí va la URL del endpoint de backend
                 parametros,
                 { response ->
-                    // Manejar la respuesta exitosa del backend
                     Toast.makeText(context, "Reserva creada con éxito", Toast.LENGTH_LONG).show()
                 },
                 { error ->
-                    // Manejar el error
                     error.networkResponse?.let {
                         val statusCode = it.statusCode
                         if (statusCode == 400) {
@@ -156,6 +184,12 @@ class Crearreserva : Fragment() {
                 val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
                 val formattedDate = dateFormat.format(selectedDate.time)
                 text.setText(formattedDate)
+
+                if (esFechaEntrada) {
+                    fechaEntrada = selectedDate
+                } else {
+                    fechaSalida = selectedDate
+                }
             }
         }, year, month, day)
 
@@ -176,6 +210,12 @@ class Crearreserva : Fragment() {
 
             val formattedTime = String.format(Locale.getDefault(), "%02d:%02d", selectedHour, selectedMinute)
             text.setText(formattedTime)
+
+            if (esHoraEntrada) {
+                horaEntrada = selectedTime
+            } else {
+                horaSalida = selectedTime
+            }
         }, currentHour, currentMinute, true)
 
         timePickerDialog.show()
